2eb4c69a08c2b9f43ad34956a5f94939cbaa8f76
dqarias <dino.quispea@gmail.com>
2022-09-15 16:38:52 -0500

    Update associations andperform some CRUD methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
-    belongs_to :user, foreign_key: "author_id", class_name: "User"
-    belongs_to :post, foreign_key: "post_id"
+  belongs_to :author, foreign_key: 'author_id', class_name: 'User'
+  belongs_to :post, foreign_key: 'post_id'
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-    belongs_to :user , foreign_key: "author_id", class_name: "User"
-    has_many :comments
-    has_many :likes
+  belongs_to :author, foreign_key: 'author_id', class_name: 'User'
+  has_many :comments
+  has_many :likes
 end
