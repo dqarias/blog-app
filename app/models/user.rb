e79db6692030ce7b888ac7f03583fbcd6e0513ab
dqarias <dino.quispea@gmail.com>
2022-09-15 17:12:04 -0500

    Add a method on User that return the most recent post

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   has_many :likes
+
+  def last_post
+    Post.where(author: self).order(created_at: :desc).limit(3)
+  end
 end
