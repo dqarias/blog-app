3d4d4237f98677fe85775383d205764ddf71897a
dqarias <dino.quispea@gmail.com>
2022-09-16 16:43:16 -0500

    Add custom method for like

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :author, foreign_key: 'author_id', class_name: 'User'
   belongs_to :post, foreign_key: 'post_id'
+
+  def update_like_counter
+    Post.increment!(:likes_counter)
+  end
 end
