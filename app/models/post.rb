1c6c01ea88b296bdf740e4e200372220a09d6097
dqarias <dino.quispea@gmail.com>
2022-09-16 16:41:14 -0500

    Add suxtom method for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, foreign_key: 'author_id', class_name: 'User'
   has_many :comments
   has_many :likes
+
+  def update_post_counter 
+    author.increment!(:posts_counter)
+  end
+
+  def last_comments 
+    Comments.where(post: self).order(created_at: :desc).limit(5)
+  end
 end
