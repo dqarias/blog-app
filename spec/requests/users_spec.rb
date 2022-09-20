ecdecc605c4227c3f57beac40aea10e9536cc052
dqarias <dino.quispea@gmail.com>
2022-09-20 17:17:19 -0500

    Add tests for users action index and show

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,18 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
+  describe 'GET /' do
+    
     it 'returns http success' do
       get '/users'
       expect(response).to have_http_status(:success)
     end
+
+    it 'renders index template' do
+      get '/users'
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes the correct text on its body' do
+      get '/users'
+      expect(response.body).to include('Here is a list of users')
+    end
+
   end
 
-  describe 'GET /show' do
+  describe 'GET /:id' do
     it 'returns http success' do
       newUser = User.create(name: 'Dino', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Peru.')
-      get "/users/#{user.id}"
+      get "/users/#{newUser.id}"
       expect(response).to have_http_status(:success)
     end
+
+    it 'renders show template' do
+      get "/users/1"
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the correct text on its body' do
+      get '/users/1'
+      expect(response.body).to include('Here is an specific user')
+    end
+
   end
 end
