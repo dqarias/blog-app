87e9b8428c7d44f53fec1760f3828a9dcb1b3dc6
dqarias <dino.quispea@gmail.com>
2022-09-20 13:10:56 -0500

    Update the request users_specs generate automatically for index and show actions

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,14 +3,15 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/users/index'
+      get '/users'
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /show' do
     it 'returns http success' do
-      get '/users/show'
+      newUser = User.create(name: 'Dino', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Peru.')
+      get "/users/#{user.id}"
       expect(response).to have_http_status(:success)
     end
   end
