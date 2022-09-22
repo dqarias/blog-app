69694e433080ffd4a34e58ead4d6bc0d03e639cb
dqarias <dino.quispea@gmail.com>
2022-09-22 12:06:01 -0500

    Update post controller show  action to display  in the view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,14 @@ class PostsController < ApplicationController
   def index
     @params = params[:user_id]
     @user_by_id = User.find(@params)
-    @post_by_user = Post.where(author_id: @params)
-    @comment_by_post = Comment.where(post: @post_by_user)
+    @posts_by_user = Post.where(author_id: @params)
+    @comment_by_post = Comment.where(post: @posts_by_user)
     
   end
 
-  def show; end
+  def show
+    @params_show = params[:id]
+    @post_by_user= Post.where(author_id: params[:user_id], id: params[:id])
+    @comment_by_post = Comment.where(post: @post_by_user)
+  end
 end
