b1d1ea1b6dc2791c5c7c0ad3a491a3647ca4243d
dqarias <dino.quispea@gmail.com>
2022-09-22 11:23:25 -0500

    Update post controller index action to show in the view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @params = params[:user_id]
+    @user_by_id = User.find(@params)
+    @post_by_user = Post.where(author_id: @params)
+    @comment_by_post = Comment.where(post: @post_by_user)
+    
+  end
 
   def show; end
 end
