1561253e8939a0a1d826fefe3c4f88028f0f284f
dqarias <dino.quispea@gmail.com>
2022-09-24 14:26:09 -0500

    Add new action to create a new comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'comments/new'
+  get 'comments/create'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:new, :create]
+    end
   end
 
   # Defines the root path route ("/")
