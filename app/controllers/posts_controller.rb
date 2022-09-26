b9e8fd5cd69a50e0767c683c37bf62c17360cd29
dqarias <dino.quispea@gmail.com>
2022-09-26 18:54:55 -0500

    Add includes(:author) to Post and Comment for removing n+1 problem show  action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
   def show
     @params_show = params[:id]
     @post_by_user = Post.where(author_id: params[:user_id], id: params[:id])
-    @comment_by_post = Comment.where(post: @post_by_user)
+    @comment_by_post = Comment.where(post: @post_by_user).includes(:author)
   end
 
   def new
