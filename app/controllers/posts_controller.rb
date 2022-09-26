c03c5851d198f2da6322b380511a2fd60ad882d9
dqarias <dino.quispea@gmail.com>
2022-09-26 12:58:03 -0500

    Add includes(:author) to Post and Comment for removing n+1 problem index action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,9 @@ class PostsController < ApplicationController
   def index
     @params = params[:user_id]
     @user_by_id = User.find(@params)
-    @post_by_user = Post.where(author_id: @params)
+    @post_by_user = Post.where(author_id: @params).includes(:author)
     @posts_by_user1 = Post.find_by(author_id: @params)
-    @comments_by_user = @posts_by_user1.last_comments
+    @comments_by_user = @posts_by_user1.last_comments.includes(:author)
   end
 
   def show
