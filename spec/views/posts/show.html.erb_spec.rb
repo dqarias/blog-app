67725a79efdf0d094f3f589b410fa230ff5555bc
Fabien <fabienswift@gmail.com>
2022-09-27 16:21:36 -0400

    Add tests for views, show post title, post author, comments_counter and likes_counter

--- spec/views/posts/show.html.erb_spec.rb
+++ spec/views/posts/show.html.erb_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe 'posts/show.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'User Show Page', type: :feature do
+  before :each do
+    @user = User.create(name: 'Tom', photo: 'https://i.pravatar.cc/150?img=8',
+      bio: "Teacher from Mexico", posts_counter: 0)
+    @first_post = Post.create(author: @user, title: 'Hello', text: 'This is my first post')
+    @second_post = Post.create(author: @user, title: 'Hello1', text: 'This is my second post')
+    @first_comment = Comment.create(post: @first_post, author: @user, text: 'Hi Tom!' )
+    @second_comment = Comment.create(post: @first_post, author: @user, text: 'Hi Tom!' )
+    @third_comment = Comment.create(post: @first_post, author: @user, text: 'Hi Tom!' )
+    @fourth_comment = Comment.create(post: @first_post, author: @user, text: 'Hi Tom!' )
+    @first_like = Like.create(post: @first_post, author: @user)
+
+    visit "/users/#{@user.id}/posts"
+  end
+  it 'show post title, post author, comments_counter and likes_counter' do
+    expect(page).to have_content(@first_post.title)
+    expect(page).to have_content(@first_post.author.name)
+    expect(page).to have_content("Comments: #{@first_post.comments_counter}")
+    expect(page).to have_content("Likes: #{@first_post.likes_counter}")
+  end
+
 end
